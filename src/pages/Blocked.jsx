a2196b814ac9d720f061dd815c1e7c574ba64532
Bassam <78672415+bassam-st@users.noreply.github.com>
2025-12-24 02:55:49 +0300

    Create Blocked.jsx

---
+++ src/pages/Blocked.jsx
@@ -0,0 +1,12 @@
+export default function Blocked() {
+  return (
+    <div className="container">
+      <div className="card col">
+        <h2>تم إيقاف حسابك</h2>
+        <small>
+          تم حظر رقمك من الإدارة. إذا كان هناك خطأ تواصل مع الإدارة.
+        </small>
+      </div>
+    </div>
+  );
+}
