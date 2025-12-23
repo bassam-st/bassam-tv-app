420231bb2db54a1c89b1dd30128ec5c345f58b43
Bassam <78672415+bassam-st@users.noreply.github.com>
2025-12-23 06:00:32 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,66 @@
+const CACHE_NAME = 'live-sports-v1'
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/favicon.svg'
+]
 
+// التثبيت
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache)
+      })
+  )
+  self.skipWaiting()
+})
+
+// التفعيل
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName)
+          }
+        })
+      )
+    })
+  )
+  self.clients.claim()
+})
+
+// جلب البيانات
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') {
+    return
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response
+        }
+
+        return fetch(event.request).then(response => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response
+          }
+
+          const responseToCache = response.clone()
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache)
+            })
+
+          return response
+        })
+      })
+      .catch(() => {
+        return caches.match('/index.html')
+      })
+  )
+})
