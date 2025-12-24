e297d7d14bb90bf5b97805434f270f8182e7a1d8
Bassam <78672415+bassam-st@users.noreply.github.com>
2025-12-24 04:10:24 +0300

    Update Blocked.jsx

--- src/pages/Blocked.jsx
+++ src/pages/Blocked.jsx
@@ -1,12 +1,3 @@
 export default function Blocked() {
-  return (
-    <div className="container">
-      <div className="card col">
-        <h2>تم إيقاف حسابك</h2>
-        <small>
-          تم حظر رقمك من الإدارة. إذا كان هناك خطأ تواصل مع الإدارة.
-        </small>
-      </div>
-    </div>
-  );
+  return <div className="screen">تم إيقاف حسابك</div>;
 }
